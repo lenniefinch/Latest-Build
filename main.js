02d62fcc2c497d78a1d4efc60348243863fd9b5c
lenniefinch <finch.leonard@gmail.com>
2020-07-10 15:27:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+//JS Nav & Burger Animation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 1
+        }s`;
+      }
+    });
+
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
+
+//Hulk
+$("a[href='#bottom']").click(function () {
+  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+  return false;
+});
